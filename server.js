ed826b0ac573194aeb6a3361e3abac8af58f8a9f
khajavali <157942120+khajavali8@users.noreply.github.com>
2025-01-16 14:52:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
-import authorizationUser from './middlewares/auth.js';
+import AuthMiddleware from './middlewares/auth.js';
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(authorizationUser)
-app.use('/userData', userRoutes);
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+app.use(AuthMiddleware);
 
-const PORT = process.env.PORT
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 4000;
 connectDB().then(()=>{
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
