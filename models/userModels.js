2d6a4c5c6f4b12788b814aeef034cd4611bf0b01
khajavali <157942120+khajavali8@users.noreply.github.com>
2025-01-16 14:51:09 +0530

    Update userModels.js

--- models/userModels.js
+++ models/userModels.js
@@ -2,32 +2,38 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  userName: {
+  name: {
     type: String,
-    required: true
+    
+  },
+  email: {
+    type: String,
+   
+    unique: true
   },
   password: {
     type: String,
-    required: true
+    
   },
-  oldPassword: {
-    type: String,
-
+  age: {
+    type: Number
+    
   },
-  newPassword: {
+  oldpassword:{
     type: String,
-  
+  },
+  oldpassword:{
+    type: String,
   },
 }, {
   timestamps: true
 });
 
-
 userSchema.pre('save', async function (next) {
-  const salt = await bcrypt.genSalt(15);
+  const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
 
-export default mongoose.model('harsha1', userSchema);
+export default mongoose.model('User', userSchema);
